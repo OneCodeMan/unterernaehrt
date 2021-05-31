8025b5f4c657f3ba19a58cb8b6b8d92267319a9b
dave gumba <dave_gumba@yahoo.com>
2021-05-30 20:41:48 -0400

    Initial Commit

---
+++ Sandy/SandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandyApp.swift
+//  Sandy
+//
+//  Created by Dave Gumba on 2021-05-30.
+//
+
+import SwiftUI
+
+@main
+struct SandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
